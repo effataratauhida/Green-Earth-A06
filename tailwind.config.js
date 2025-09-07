7c0cffa8018e3971594ae01bfb11084f1c7e03b4
Effat Ara Tauhida <effataratauhida2020@gmail.com>
2025-09-08 01:59:41 +0600

    added basic html and assets files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./*.html",         // সব HTML ফাইল স্ক্যান করবে
+//     "./src/**/*.{js}",  // যদি src ফোল্ডারে JS থাকে
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
